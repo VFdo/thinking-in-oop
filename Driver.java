7f4c389232f5acdb687781a76eae9c7e87e03f51
Vidu Fernando <vidufernando98@gmail.com>
2026-07-01 20:09:11 +0530

    adding try-catch

--- Driver.java
+++ Driver.java
@@ -13,28 +13,32 @@ public class Driver{
         Scanner sc = new Scanner(System.in);
         Shape shape = null;
 
-        System.out.println("Select a shape: \n\t1. Square\n\t2. Rectangle\n\t3. Circle");
-        int shapeSelection = sc.nextInt();
+        try{
+            System.out.println("Select a shape: \n\t1. Square\n\t2. Rectangle\n\t3. Circle");
+            int shapeSelection = sc.nextInt();
 
-        switch (shapeSelection) {
-            case 1:
-                shape = factory.createSquare(input.lengthInput());
-                break;
-            case 2:
-                shape = factory.createRectangle(
-                    input.heightInput(), 
-                    input.widthInput()
-                );
-                break;
-            case 3:
-                shape = factory.createCircle(input.radiusInput());
-                break;
-            default:
-                System.out.println("Invalid Input! Try Again!");
-                break;
+            switch (shapeSelection) {
+                case 1:
+                    shape = factory.createSquare(input.lengthInput());
+                    break;
+                case 2:
+                    shape = factory.createRectangle(
+                        input.heightInput(), 
+                        input.widthInput()
+                    );
+                    break;
+                case 3:
+                    shape = factory.createCircle(input.radiusInput());
+                    break;
+                default:
+                    System.out.println("Invalid Input! Try Again!");
+                    System.exit(0);
+            }
+            print.printDetails(shape);
+            sc.close();
         }
-
-        print.printDetails(shape);
-        sc.close();
+        catch(Exception ex){
+            System.out.println("An error occured! Try Again!");
+        }
     }
 }
